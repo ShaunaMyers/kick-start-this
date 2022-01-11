5f3141a6cb54985680f5e8363a23da481e044bbd
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 12:08:40 -0700

    Create & add Products component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { sampleProducts } from '../../sample_product_data';
+import Products from '../Products/Products';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  const returnProducts = () => {
+    return sampleProducts.rows;
+  }
+
+  useEffect(() => {
+    const data = returnProducts();
+    setProducts(data)
+  }, [])
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Kickstart This</h1>
       </header>
-    </div>
+      <section>
+        <Products products={products}/>
+      </section>
+    </main>
   );
 }
 
