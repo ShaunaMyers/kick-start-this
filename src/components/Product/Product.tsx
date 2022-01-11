123d47bb9a735e8499c0e7b3c66c258b8e55c513
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 16:18:19 -0700

    Return MUI components for each product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,11 +1,35 @@
-function Product({ image, title, description }) {
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
+function Product({ image, title, description, funds_goal }) {
     return (
-      <section className="App">
-        <img src={image}/>
-        <h2>{title}</h2>
-        <h4>{description}</h4>
-      </section>
+      <Grid item xs={6}>
+        <Card sx={{ maxWidth: 450}}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='250'
+              image={image}
+              alt={'image of ' + title}
+            />
+            <CardContent>
+              <Typography variant='h5' component='div'>
+                {title}
+              </Typography>
+              <Typography variant='body2' color="text.secondary">
+                {description}
+              </Typography>
+              <Typography variant='h6' component='div'>
+                 {funds_goal}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
     );
   }
   
