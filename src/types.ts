be7ebcae7f5528b57d1a2e741f4e8716ac69ff8c
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 16:17:20 -0700

    Create interface in types file

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface AllProducts {
+   product_id: number, 
+   title: string, 
+   description: string, 
+   funds_goal: number, 
+   funds_raised: number, 
+   images: string[], 
+   creator_name: string, 
+   creator_email: string,
+}
