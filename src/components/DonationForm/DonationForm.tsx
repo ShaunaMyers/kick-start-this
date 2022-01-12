a45f4a1bad3159cd7b0845e9cefbad5d28dcad4e
Shauna Myers <shaunamyers26@gmail.com>
2022-01-12 16:28:33 -0700

    Add stripe public key with loadStripe call

--- src/components/DonationForm/DonationForm.tsx
+++ src/components/DonationForm/DonationForm.tsx
@@ -1,19 +1,23 @@
-import { useLocation, useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const DonationForm = () => {
 
     const { id, title } = useParams();
-    // const location = useLocation();
-    // const { title } = location.state;
-    // const [productTitle, setProductTitle] = useState([]);
 
-    // useEffect(() => {
-    //     setProductTitle(title);
-    // }, [])
+    const PUBLIC_KEY= "pk_test_51KHG52GKDS6iRdpblhIx1DXqNDKYFMHMyDzZbnv8ZSZBsLQ8E9kclzy3B2kWgGs6XQ7CmAzRnq2MwI4d9rnncZGf00VDPjSx92";
+
+    const stripeTestPromis = loadStripe(PUBLIC_KEY);
 
     return (
-        <h6>Title: {title}</h6>
+        <>
+            <Typography variant="h4">Donation Summary</Typography>
+            <Typography variant="h6">We thank you for choosing to fund {title}!</Typography>
+            <Divider />
+            <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment Method</Typography>
+        </>
     );
 }
 
