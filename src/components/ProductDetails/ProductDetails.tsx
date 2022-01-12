94d8d3c3f0ff0d6b0cd5f4c56bdcbdc3ee76aaa3
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 19:37:40 -0700

    Add link to return home

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { sampleProducts } from '../../sample_product_data';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { sampleProducts } from "../../sample_product_data";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 function ProductDetails() {
 
@@ -26,25 +25,30 @@ function ProductDetails() {
         {singleProduct.images &&
             <Card sx={{ maxWidth: 600}}>
                 <CardMedia
-                  component='img'
-                  height='250'
+                  component="img"
+                  height="250"
                   image={singleProduct.images[0]}
-                  alt={'image of ' + singleProduct.title}
+                  alt={"image of " + singleProduct.title}
                 />
                 <CardContent>
-                  <Typography variant='h5' component='div'>
+                  <Typography variant="h5" component="div">
                     {singleProduct.title}
                   </Typography>
-                  <Typography variant='body2'>
+                  <Typography variant="body2">
                     {singleProduct.description}
                   </Typography>
-                  <Typography variant='h6' component='div'>
+                  <Typography variant="h6" component="div">
                     Funds Goal: ${singleProduct.funds_goal}
                   </Typography>
-                  <Typography variant='h6' component='div'>
+                  <Typography variant="h6" component="div">
                     Funds Raised: ${singleProduct.funds_raised}
                   </Typography>
-                  <Link to='/donate'><button>Donate</button></Link>
+                  <Typography>
+                    <Link to="/donate"><button>Donate</button></Link>
+                  </Typography>
+                  <Typography>
+                    <Link to="/">Return to browse all products</Link>
+                  </Typography>
                 </CardContent>
             </Card>
         }
