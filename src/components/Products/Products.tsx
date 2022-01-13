7538c7eecef75a76a60f7ce2b991f3e5b93ca801
Shauna Myers <shaunamyers26@gmail.com>
2022-01-13 11:43:16 -0700

    Pass isAdmin & delete func props

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -2,10 +2,10 @@ import Product from '../Product/Product';
 import Grid from '@material-ui/core/Grid';
 
 
-const Products = ({ productsList, isAdmin }) => {
+const Products = ({ productsList, isAdmin, handleDeleteProduct }) => {
 
     const allProducts = productsList.map(item => {
-       return <Product key={item.product_id} id={item.product_id} image={item.images[0]} title={item.title} description={item.description} funds_goal={item.funds_goal} isAdmin={isAdmin}/>
+       return <Product key={item.product_id} id={item.product_id} image={item.images[0]} title={item.title} description={item.description} funds_goal={item.funds_goal} isAdmin={isAdmin} handleDeleteProduct={handleDeleteProduct}/>
     })
 
     return (
