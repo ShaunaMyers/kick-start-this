8acd980d93ed53ce271ff2500311e1508ee7a674
Shauna Myers <shaunamyers26@gmail.com>
2022-01-12 20:18:44 -0700

    Add onChange to TextFields to set state

--- src/components/CreateProduct/CreateProduct.tsx
+++ src/components/CreateProduct/CreateProduct.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
+import { addSingleProduct } from '../../apiCalls';
 import { FormControl, TextField, Button } from '@material-ui/core';
 
 const CreateProduct = () => {
 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [fundsGoal, setFundsGoal] = useState('');
+    const [images, setImages] = useState([]);
+    const [creatorName, setCreatorName] = useState('');
+    const [creatorEmail, setCreatorEmail] = useState('');
+
+    const onAddToProducts = (e) => {
+        e.preventDefault();
+        addSingleProduct()
+    }
+
     return (
         <>
             <FormControl>
-                <TextField label="Full Name" color="warning" focused/>
-                <TextField label="Email" color="success" focused />
-                <TextField label="Product Title" variant="standard" color="warning" focused/>
-                <TextField label="Product Description" variant="standard" color="warning" focused/>
-                <TextField label="Funds Needed" variant="standard" color="warning" focused/>
-                <TextField label="Product Title" variant="standard" color="warning" focused/>
+                <TextField onChange={(e) => setCreatorName(e.target.value)} label="Full Name" color="warning" value={creatorName} focused/>
+                <TextField onChange={(e) => setCreatorEmail(e.target.value)} label="Email" color="success" value={creatorName} focused />
+                <TextField onChange={(e) => setTitle(e.target.value)} label="Product Title" variant="standard" color="warning" value={title} focused/>
+                <TextField onChange={(e) => setDescription(e.target.value)} label="Product Description" variant="standard" color="warning" value={description} focused/>
+                <TextField onChange={(e) => setFundsGoal(e.target.value)} label="Funds Needed" variant="standard" color="warning" value={fundsGoal} focused/>
                 <input accept="image/*" style={{ display: 'none' }} id="raised-button-file" multiple type="file" />
                 <label htmlFor="raised-button-file">
                 <Button variant="raised" component="span">
