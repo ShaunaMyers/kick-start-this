aa494d686b62b77ea26b46b4a0e2b721f71a57c0
Shauna Myers <shaunamyers26@gmail.com>
2022-01-12 21:53:02 -0700

    Add timer & validate inputs

--- src/components/CreateProduct/CreateProduct.tsx
+++ src/components/CreateProduct/CreateProduct.tsx
@@ -6,31 +6,62 @@ const CreateProduct = () => {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [fundsGoal, setFundsGoal] = useState('');
+    const [fundsGoal, setFundsGoal] = useState(0);
+    const [singleImage, setSingleImage] = useState('');
     const [images, setImages] = useState([]);
     const [creatorName, setCreatorName] = useState('');
     const [creatorEmail, setCreatorEmail] = useState('');
+    const [timer, setTimer] = useState('')
+    const [error, setError] = useState('');
 
-    const onAddToProducts = (e) => {
+    const validateInputs = () => {
+        console.log('single image', singleImage)
+        if (!title || !description || !fundsGoal || !images.length || !creatorName || !creatorEmail) {
+            setError('Please fill out all the fields')
+            setTimer(setTimeout(() => setError(''), 5000))
+            return;
+        } 
+        
+        // else {
+        //     setError('')
+        // }
+    }
+
+    const onAddProduct = (e) => {
+        validateInputs();
         e.preventDefault();
-        addSingleProduct()
+        const newProduct = { title, description, funds_goal: parseInt(fundsGoal), images: images, creator_name: creatorName, creator_email: creatorEmail }
+        console.log(newProduct)
+        // addSingleProduct(newProduct);
+    }
+
+    const handleAddImage = (e) => {
+        e.preventDefault();
+        if (images.length < 4) {
+            images.length ? setImages([...images, singleImage])
+            : setImages([singleImage]);
+            setSingleImage('');
+        } else {
+            setError('You have reached the max amount of photo uploads')
+            setTimer(setTimeout(() => setError(''), 5000))
+            setSingleImage('');
+        }
     }
 
     return (
         <>
             <FormControl>
                 <TextField onChange={(e) => setCreatorName(e.target.value)} label="Full Name" color="warning" value={creatorName} focused/>
-                <TextField onChange={(e) => setCreatorEmail(e.target.value)} label="Email" color="success" value={creatorName} focused />
+                <TextField onChange={(e) => setCreatorEmail(e.target.value)} type="email" label="Email" color="success" value={creatorEmail} focused />
                 <TextField onChange={(e) => setTitle(e.target.value)} label="Product Title" variant="standard" color="warning" value={title} focused/>
                 <TextField onChange={(e) => setDescription(e.target.value)} label="Product Description" variant="standard" color="warning" value={description} focused/>
                 <TextField onChange={(e) => setFundsGoal(e.target.value)} label="Funds Needed" variant="standard" color="warning" value={fundsGoal} focused/>
-                <input accept="image/*" style={{ display: 'none' }} id="raised-button-file" multiple type="file" />
-                <label htmlFor="raised-button-file">
-                <Button variant="raised" component="span">
-                    Upload
+                <input onChange={(e) => setSingleImage(e.target.value)} type="url" value={singleImage}/>
+                <Button onClick={(e) => handleAddImage(e)} variant="raised" component="span">
+                    Add Photo
                 </Button>
-                </label> 
-                <Button onClick={(e) => onAddToProduct(e)} variant="contained" type="submit" color="seconday">Create Product</Button>
+                <Button onClick={(e) => onAddProduct(e)} variant="contained" type="submit" color="seconday">Create Product</Button>
+                {error && <p>{error}</p>}
             </FormControl>
         </>
     )
