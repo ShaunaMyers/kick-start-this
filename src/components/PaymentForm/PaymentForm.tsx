577803b3b5de6e87e8328276fb7631cceff0ffef
Shauna Myers <shaunamyers26@gmail.com>
2022-01-12 17:12:10 -0700

    Install axios & use for post request to stripe

--- src/components/PaymentForm/PaymentForm.tsx
+++ src/components/PaymentForm/PaymentForm.tsx
@@ -1,6 +1,41 @@
+import { useState } from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { stripVTControlCharacters } from 'util';
+import axios from "axios";
+
+
 const PaymentForm = () => {
+    const [succesful, setSuccessful] = useState(false);
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const [error, paymentMethod] = await stripe.createPaymentMethod({
+            type: "card",
+            card: elements?.getElement(CardElement)
+        })
+    }
+
+    if (!error) {
+        try {
+            const { id } = paymentMethod;
+            const response = await axios.post("http://localhost:4000/payment", {
+                amount: 1000,
+                id
+            })
+        } catch {
+            if(Response.data.success) {
+                console.log("Your payment was successful!");
+                setSuccessful(true);
+            }
+        }
+    }
+
     return(
-        
+        <div>
+
+        </div>
     )
 }
 
